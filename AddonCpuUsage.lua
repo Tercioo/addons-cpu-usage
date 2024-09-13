6be86131307d1b182ce8419354e95ab5eab2f658
Tercio Jose <terciob19@hotmail.com>
2024-09-13 18:06:36 -0300

    Upvalues update

--- AddonCpuUsage.lua
+++ AddonCpuUsage.lua
@@ -12,11 +12,10 @@ local GameTooltip = GameTooltip
 --local debugmode = true
 local debugmode = false
 
-local GetAddOnInfo   = C_AddOns.GetAddOnInfo
---local GetFramerate   = GetFramerate
-local GetNumAddOns   = C_AddOns.GetNumAddOns
-local GetAddOnCPUUsage       = GetAddOnCPUUsage
-local UpdateAddOnCPUUsage    = UpdateAddOnCPUUsage
+local GetAddOnInfo   		= C_AddOns and C_AddOns.GetAddOnInfo or GetAddOnInfo
+local GetNumAddOns   		= C_AddOns and C_AddOns.GetNumAddOns or GetNumAddOns
+local GetAddOnCPUUsage		= GetAddOnCPUUsage
+local UpdateAddOnCPUUsage	= UpdateAddOnCPUUsage
 
 local CPUResetUsage = ResetCPUUsage
 ResetCPUUsage = function()
